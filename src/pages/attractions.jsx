09c0f4d29f0fcfcbbbd0c756a53d9cfb02608b35
Your Name <you@example.com>
2025-01-24 12:59:01 +0300

    commit

---
+++ src/pages/attractions.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import AttractionsSection from "../components/AttractionsSection"
+
+function Contact() {
+  return (
+    <div className='App'>
+        <Header />
+        <AttractionsSection />
+        <Footer />
+    </div>
+  );
+}
+
+export default Contact;
